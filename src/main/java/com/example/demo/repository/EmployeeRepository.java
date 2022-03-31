b96a05f751e7965cfc114afba5dcd686d8e8da39
Alok Kumar <59380269+AlokSP@users.noreply.github.com>
2022-03-31 22:38:50 +0530

    make it better

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-    Employee findByName(String name);
+    
 }
